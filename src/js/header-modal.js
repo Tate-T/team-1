d1074bc27d1765941acaba8d1a4c6b8451110018
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-02-16 01:16:13 +0200

    fix

---
+++ src/js/header-modal.js
@@ -0,0 +1,29 @@
+const headerLink = document.querySelector(".header__minitext");
+
+headerLink.addEventListener("click", () => {
+  document.querySelector(".backdrope").style.display = "flex";
+  document.body.style.overflow = "hidden";
+
+  document.querySelector(".modale__text").style.display = "flex";
+  document.querySelector(".modale__minitext").style.display = "flex";
+  document.querySelector(".modale__button").style.display = "flex";
+  document.querySelector(".modale__input").style.display = "flex";
+  document.querySelector(".modale__thank").style.display = "none";
+});
+
+const headerCan = document.querySelector(".modale__cancel");
+
+headerCan.addEventListener("click", () => {
+    document.querySelector(".backdrope").style.display = "none";
+    document.body.style.overflow = "scroll";
+})
+
+const headerThank = document.querySelector(".modale__button");
+
+headerThank.addEventListener("click", () => {
+    document.querySelector(".modale__text").style.display = "none";
+    document.querySelector(".modale__minitext").style.display = "none";
+    document.querySelector(".modale__button").style.display = "none";
+    document.querySelector(".modale__input").style.display = "none";
+    document.querySelector(".modale__thank").style.display = "flex";
+})
