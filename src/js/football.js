7de521d4f04709093b94a3da9f5e13caf9b058a7
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-02-16 15:51:52 +0200

    done

---
+++ src/js/football.js
@@ -0,0 +1,24 @@
+const stadium = document.querySelector(".football__stadium")
+const ball = document.querySelector(".football__ball")
+
+stadium.onclick = function(event) {
+    let fieldCoords = this.getBoundingClientRect();
+
+  let ballCoords = {
+      top: event.clientY - fieldCoords.top - stadium.clientTop - ball.clientHeight / 2,
+      left: event.clientX - fieldCoords.left - stadium.clientLeft - ball.clientWidth / 2
+    };
+    if (ballCoords.top < 0) ballCoords.top = 0;
+    if (ballCoords.left < 0) ballCoords.left = 0;
+
+    if (ballCoords.left + ball.clientWidth > stadium.clientWidth) {
+      ballCoords.left = stadium.clientWidth - ball.clientWidth;
+    }
+    if (ballCoords.top + ball.clientHeight > stadium.clientHeight) {
+      ballCoords.top = stadium.clientHeight - ball.clientHeight;
+    }
+    ball.style.left = 0;
+    ball.style.top = 0;
+
+    ball.style.transform = `translate(${ballCoords.left}px, ${ballCoords.top}px)`
+  }
