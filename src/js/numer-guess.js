4658fbb54a4d88757becc78bb83cea710ff6f4db
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-02-21 18:20:09 +0200

    fix

--- src/js/numer-guess.js
+++ src/js/numer-guess.js
@@ -6,13 +6,15 @@ document.querySelector(".quess__button").addEventListener("click", (e) => {
     console.log(rundNum);
     console.log(Number(inputQuess.value));
     console.log(Number(inputQuess.value) == rundNum);
-    document.querySelector(".quess__span").textContent = ` ${rundNum}`;
     if(Number(inputQuess.value) == rundNum){
         document.querySelector(".quess__answer").style.display = "flex"
         document.querySelector(".quess__answer").style.color = "#039900";
+        document.querySelector(".quess__answer").innerHTML = 'Вітаю, ви вгадали число!<span class="quess__span"> </span>';
+        document.querySelector(".quess__span").textContent = ` ${rundNum}`;
     }else{
          document.querySelector(".quess__answer").style.display = "flex"
         document.querySelector(".quess__answer").style.color = "#900";
-
+        document.querySelector(".quess__answer").innerHTML = 'Ви програли, комп’ютер загадав <span class="quess__span"> </span>';
+        document.querySelector(".quess__span").textContent = ` ${rundNum}`;
     }
 });
